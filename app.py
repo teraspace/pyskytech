7b3de0888c31f45fccd5a8abdf3958434ba800cd
Carlos Patiño <cpatino@fingeosel.com>
2020-07-21 10:52:50 -0500

    add permanence csv

--- app.py
+++ app.py
@@ -332,6 +332,37 @@ def history_cathodics_daily():
     return send_from_directory(full_path, 'history_cathodics_daily'+owner_id+'.csv', as_attachment=True)
 
 
+@server.route('/data_science/history_permanence')
+def history_permanence():
+    print ('hello history_permanence')
+    req = request.args.to_dict(flat=True)
+    user = query_user(req)
+    permanence = query_permanence(req)
+    data_report = pd.concat([permanence])
+    data_report = data_report.sort_index()
+    data_report['DATE_ENTRY']= pd.to_datetime(data_report['DATE_ENTRY']) 
+    data_report.sort_values('DATE_ENTRY', inplace=True, ascending=False)
+    column_keys = [ 'PLATE','DATE_ENTRY','SPEED','ADDRESS','ZONE','PERMANENCE','EVENT_HOUR','X','Y','SHEET','INTERNAL_CODE','VALUE' ]
+    translaters = [x.lower() for x in column_keys]
+
+
+    
+    column_names = []
+
+    for c in column_keys:
+        column_names.append(translate(c, translaters, user.locale))
+
+    data_report = data_report[column_keys]
+    data_report.columns = column_names
+    owner_id = str(user.owner_id)
+    
+    full_path = os.path.dirname(os.path.abspath(__file__))
+    #data_report.to_excel(full_path+'/history_premanence'+owner_id+'.xlsx', index=False)
+    data_report.replace('\n','', regex=True).to_csv(full_path+'/history_premanence'+owner_id+'.csv', index=False)
+    return send_from_directory(full_path, 'history_premanence'+owner_id+'.csv', as_attachment=True)
+
+
+
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
--- reports/history.py
+++ reports/history.py
@@ -89,3 +89,46 @@ def query_historics(args):
         data_historics.columns = data_historics.columns
     conn.close()
     return data_historics    
+
+
+
+def query_permanence(args):
+    conn = cx_Oracle.connect("skytech", "skytech", "oracle.geotech.com.co/geotech", encoding='UTF-8', nencoding='UTF-8')
+    plates =  args.get('plates').replace('[','').replace(']','').replace('"', '\'')  
+    
+    
+    events = []
+    for e in args.get('events_ids').replace('[','').replace(']','').replace('"', '\'').split(','):
+            events.append(str(e))
+
+            
+    events =  str(args.get('events_ids')).replace('[','').replace(']','').replace('"', '\'')
+    # and zone in('POSTOBO BELLO') 
+   # sql = "SELECT /*+ index(historics,HISTORICS_INDEX3CDESC) */ historics.ID, historics.PLATE, historics.ADDRESS,X, Y, DATE_ENTRY as date_entry_date , DATE_SYSTEM AS date_system_date,to_char( DATE_ENTRY, 'dd/mm/yyyy hh24:mi:ss' ) as DATE_ENTRY , to_char( DATE_SYSTEM, 'dd/mm/yyyy hh24:mi:ss' ) as DATE_SYSTEM ,to_timestamp( to_char(DATE_ENTRY, 'dd/mm/yyyy hh24:mi:ss'), 'dd/mm/yyyy hh24:mi:ss' ) as date_time_entry ,ORIENTATION,SPEED,historics.MOBILE_ID,historics.EVENT_ID,'' as VALUE,'posicion_gps' as DESCRIPTION_EVENT,'posicion_gps' as event_name,BATTERY,SHEET,null as camera  ,historics.INTERNAL_CODE  FROM \"HISTORICS\" left join  \"OWNER_PLATES\" pl ON pl.plate=\"HISTORICS\".plate and pl.owner_id = :owner_id left join  \"OWNER_EVENTS\" ev ON ev.event_id=\"HISTORICS\".event_id and ev.owner_id = :owner_id left join  \"OWNER_DRIVERS\" dri ON dri.id=pl.OWNER_DRIVER_ID  WHERE  date_entry between       to_date(:fecha_inicial, 'dd/mm/yyyy hh24:mi:ss') and     to_date(:fecha_final, 'dd/mm/yyyy hh24:mi:ss') and ev.PAGE = 1  AND   \"HISTORICS\".plate in(" + plates + ")  and \"HISTORICS\".event_id in(" + events + ")  ORDER BY \"HISTORICS\".plate ASC, date_entry desc"
+    sql = "SELECT  \"PERMANENCE_REPORTS\".* FROM \"PERMANENCE_REPORTS\" WHERE ( plate in(" + plates + ") and date_entry>= to_date(:fecha_inicial,'dd/mm/yyyy hh24:mi:ss') and date_entry <= to_date(:fecha_final,'dd/mm/yyyy hh24:mi:ss') ) ORDER BY plate, date_entry desc"
+    curs = conn.cursor()
+    curs.prepare(sql)
+
+    curs.execute(None, 
+                 {
+                  'fecha_inicial': args.get('fecha_inicial') ,  
+                  'fecha_final': args.get('fecha_final')
+                 
+                 }
+                 
+                
+                )
+    df2 = DataFrame(curs.fetchall())
+    data_historics = df2
+    c = 0
+    column_names = []
+    for column in curs.description:
+        column_names.append(curs.description[c][0])
+        c += 1
+
+    try:
+        data_historics.columns = column_names
+    except:
+        data_historics.columns = data_historics.columns
+    conn.close()
+    return data_historics    
